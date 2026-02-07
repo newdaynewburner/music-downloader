3d3c4b03147f5c7885ca9982b0d2bdb07c4a4e9b
Brandon Hammond <newdaynewburner@gmail.com>
2026-02-07 05:20:20 -0600

    Repo cleanup!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # music-downloader
-A simple music downloader written in Python for downloading songs and albums from YouTube. Has both a CLI interface as well as a graphical one. 
+A simple music downloader written in Python for downloading songs and albums from YouTube. Has a graphical interface made with tkinter.  
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="music-downloader",
-    version="0.1",
+    version="0.2",
     url="https://github.com/newdaynewburner/music-downloader",
     author="Brandon Hammond",
     author_email="newdaynewburner@gmail.com",
-    description="A simple music downloader written in Python for downloading songs and albums from YouTube. Has both a CLI interface as well as a graphical one.",
+    description="A simple music downloader written in Python for downloading songs and albums from YouTube.",
     packages=find_packages(),
     install_requires=[],
 )
